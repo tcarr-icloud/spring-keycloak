f7c26fcb843a00bab234f0b1703d20404c76f829
tcarr-icloud <carr.timothy@gmail.com>
2025-10-22 17:50:28 -0600

    reformat

--- src/test/java/com/example/springkeycloak/SpringKeycloakApplicationTests.java
+++ src/test/java/com/example/springkeycloak/SpringKeycloakApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringKeycloakApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
+  @Test
+  void contextLoads() {}
 }
